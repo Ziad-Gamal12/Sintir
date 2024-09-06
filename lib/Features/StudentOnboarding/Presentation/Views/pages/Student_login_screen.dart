9c3472b38e7dc2970971069e007bf002c065d633
omarali <omaraliabdelaziz250@gmail.com>
2024-09-06 14:20:26 +0300

    student login sceen

---
+++ lib/Features/StudentOnboarding/Presentation/Views/pages/Student_login_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StudentLoginPage extends StatelessWidget {
+  const StudentLoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
