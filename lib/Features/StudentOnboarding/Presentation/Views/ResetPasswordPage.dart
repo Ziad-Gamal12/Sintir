02364740519594ac1fce9442b6e5e077a3ab7a44
omarali <omaraliabdelaziz250@gmail.com>
2024-09-10 12:54:55 +0300

    reset password page

--- lib/Features/StudentOnboarding/Presentation/Views/ResetPasswordPage.dart
+++ lib/Features/StudentOnboarding/Presentation/Views/ResetPasswordPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sintir/Features/StudentOnboarding/Presentation/Views/widgets/ResetPasswordBody.dart';
 
 class ResetPasswordPage extends StatelessWidget {
   static const String routeName = "/ResetPasswordPage";
