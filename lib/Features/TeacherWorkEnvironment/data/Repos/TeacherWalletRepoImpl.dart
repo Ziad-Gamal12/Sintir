2b52d1ab39afc51e704fb025b2657dfb9215ff8a
ziad gamal <ziadgemy123@gmail.com>
2025-10-27 15:33:09 +0300

    get user data and fetch form firestore when the teacher with draw thier balance

--- lib/Features/TeacherWorkEnvironment/data/Repos/TeacherWalletRepoImpl.dart
+++ lib/Features/TeacherWorkEnvironment/data/Repos/TeacherWalletRepoImpl.dart
@@ -3,12 +3,15 @@ import 'package:sintir/Core/errors/Exceptioons.dart';
 import 'package:sintir/Core/errors/Failures.dart';
 import 'package:sintir/Core/services/DataBaseService.dart';
 import 'package:sintir/Core/utils/Backend_EndPoints.dart';
+import 'package:sintir/Features/Auth/Domain/Repos/AuthRepo.dart';
 import 'package:sintir/Features/TeacherWorkEnvironment/domain/Repos/TeacherWalletRepo.dart';
 
 class TeacherWalletRepoImpl implements TeacherWalletRepo {
   final Databaseservice databaseservice;
+  final AuthRepo authRepo;
 
-  TeacherWalletRepoImpl({required this.databaseservice});
+  TeacherWalletRepoImpl(
+      {required this.authRepo, required this.databaseservice});
 
   @override
   Future<Either<Failure, void>> updateTeacherWalletBalance(
@@ -20,6 +23,7 @@ class TeacherWalletRepoImpl implements TeacherWalletRepo {
         doc: teacherId,
         data: balance,
       );
+      await authRepo.fetchUserAndStoreLocally(uid: teacherId);
       return right(null);
     } on CustomException catch (e) {
       return left(ServerFailure(message: e.message));
